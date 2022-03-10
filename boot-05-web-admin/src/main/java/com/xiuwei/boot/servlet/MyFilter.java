5adf8fc2791570dee36c8f46c1aeeb5c86543856
zhuxiuwei <zhuxiuwei@zhuxiuweideMacBook-Pro.local>
2022-03-10 13:26:40 +0800

    原生filer

---
+++ boot-05-web-admin/src/main/java/com/xiuwei/boot/servlet/MyFilter.java
@@ -0,0 +1,27 @@
+package com.xiuwei.boot.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(urlPatterns = {"/css/*", "/images/*"})   //配置拦截路径，目前让链接静态资源。 注意一个*是servlet写法。springboot写法是**。
+public class MyFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("MyFilter初始化完成");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        log.info("MyFilter工作");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        log.info("MyFilter销毁");
+    }
+}
