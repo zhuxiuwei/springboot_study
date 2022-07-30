cf8b4f002854293f58675b96f5207b715a447e7a
zhuxiuwei <zhuxiuwei@meituan.com>
2022-07-30 16:56:54 +0800

    commnet

--- boot-10-custom-starter/xiuwei-hello-spring-boot-starter-autoconfig/src/main/java/com/xiuwei/hello/auto/HelloServiceAutoConfiguration.java
+++ boot-10-custom-starter/xiuwei-hello-spring-boot-starter-autoconfig/src/main/java/com/xiuwei/hello/auto/HelloServiceAutoConfiguration.java
@@ -15,7 +15,8 @@ public class HelloServiceAutoConfiguration {
     /*
      ！！！注意@ConditionalOnMissingBean没有写到class上，而是写到方法上。
      否则如果类上的@ConditionalOnMissingBean(HelloService.class)没有执行到，则此类上的注解"@EnableConfigurationProperties(HelloProperties.class)"也会不生效，
-     会导致客户端（如boot-10-custom-starter-test的boot.config.MyConfig）主动生成并注册HelloService时，启动App会报错HelloProperties找不到：Field helloProperties in com.xiuwei.hello.service.HelloService required a bean of type 'com.xiuwei.hello.bean.HelloProperties' that could not be found.
+     会导致客户端（如boot-10-custom-starter-test的boot.config.MyConfig）主动生成并注册HelloService时，启动App会报错HelloProperties找不到：
+     Field helloProperties in com.xiuwei.hello.service.HelloService required a bean of type 'com.xiuwei.hello.bean.HelloProperties' that could not be found.
      */
     @ConditionalOnMissingBean(HelloService.class)
     public HelloService helloService(){
